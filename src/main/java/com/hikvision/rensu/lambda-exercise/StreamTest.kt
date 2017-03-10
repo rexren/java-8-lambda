d3f414dcf272e2d36a6fc44156d0dc65a03527b9
rexren <rexrens@gmail.com>
2017-03-10 19:35:17 +0800

    init project

---
+++ src/main/java/com/hikvision/rensu/lambda-exercise/StreamTest.kt
@@ -0,0 +1,9 @@
+package com.hikvision.rensu.lambda
+
+-exercise
+
+/**
+ * Created by rensu on 17/3/10.
+ */
+class StreamTest {
+}
---
+++ src/main/java/com/hikvision/rensu/lambda/chapter1/LambdaTest.java
@@ -0,0 +1,10 @@
+package com.hikvision.rensu.lambda.chapter1;
+
+/**
+ * Created by rensu on 17/3/10.
+ */
+public class LambdaTest {
+    public static void main(String[] args) {
+
+    }
+}

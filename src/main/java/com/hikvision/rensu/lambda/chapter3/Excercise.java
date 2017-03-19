b0e9df30b4201932589147b94cbb2714440985ac
rexren <rexrens@gmail.com>
2017-03-19 17:21:56 +0800

    测试了统计所有小写字母的并行模式性能,确实比较快,但是最快的还是for循环,汗颜

--- src/main/java/com/hikvision/rensu/lambda/chapter3/Excercise.java
+++ src/main/java/com/hikvision/rensu/lambda/chapter3/Excercise.java
@@ -1,6 +1,7 @@
 package com.hikvision.rensu.lambda.chapter3;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.*;
@@ -10,19 +11,53 @@ import java.util.*;
  */
 public class Excercise {
 
-    static public int countLowCaseCharNumber(String string){
-        long number = string.chars().filter(Character::isLowerCase).count();
-//        long number = string.chars().filter(c->Character.isLowerCase(c)).count();
-        return (int)number;
+    private final String s1 = "fhfudisoahf inpGYUERTYUIOP:L<MNBVDRTYUIOL<MNBFTYUIOLKMNBVFRTYUIIUOINSFJIOHBOIGFSUIODhupsdhafiopb hfubsoha UI GUIGALNJFMIOHGUNSOUHIIfjaghiuonh usanfihUIBGIFUHSIhfuihgaio ou";
+    private final String s2 = "fdhfudisoahf inpGYUIUOINSFRTYUIKMNBVFTYUIKJNBVFTYUIKMNBFTYUIKJNBFTYUIJHBFTYUIKMJIOHBOIGFSUIODhupsdhafiopb hfubsoha UI GUIGALNJFMIOHGUNSOUHIIfjaghiuonh usanfihUIBGIFUHSIhfuihgaio ou";
+
+    static public long countLowCaseCharNumber(String string){
+        return string.chars().filter(Character::isLowerCase).count();
+    }
+
+    static public long countLowCaseCharNumberWithPara(String str){
+        return str.chars().parallel().filter(Character::isLowerCase).count();
+    }
+
+    static public long coutFor(String str){
+        long count = 0;
+        for (int i=0;i<str.length();i++){
+            if (Character.isLowerCase(str.charAt(i))){
+                count++;
+            }
+
+        }
+        return count;
+    }
+
+    @Test
+    public void testTimeFor(){
+        Assert.assertEquals(62, coutFor(s1));
+        Assert.assertEquals(63, coutFor(s2));
+    }
+
+    @Test
+    public void testTimeStream(){
+        Assert.assertEquals(62, countLowCaseCharNumber(s1));
+        Assert.assertEquals(63, countLowCaseCharNumber(s2));
+    }
+
+    @Test
+    public void testTimeWithPara(){
+        Assert.assertEquals(62, countLowCaseCharNumberWithPara(s1));
+        Assert.assertEquals(63, countLowCaseCharNumberWithPara(s2));
+
     }
 
     static public String mostUsedLowercaseChar(List<String> strings){
-       return strings.stream().max(Comparator.comparing(Excercise::countLowCaseCharNumber)).get();
-//       return strings.stream().max(Comparator.comparing(c->countLowCaseCharNumber(c)));
+        return strings.stream().max(Comparator.comparing(Excercise::countLowCaseCharNumber)).get();
     }
 
-
     @Test
+    @Ignore
     public void lowcaseTest(){
         Assert.assertEquals(0, countLowCaseCharNumber(""));
         Assert.assertEquals(1, countLowCaseCharNumber("aA"));
@@ -30,8 +65,10 @@ public class Excercise {
     }
 
     @Test
+    @Ignore
     public void mostUsedLowerCaseStirngsTest(){
         Assert.assertEquals("a", mostUsedLowercaseChar(Arrays.asList("a", "A")));
         Assert.assertEquals("afdsafsa", mostUsedLowercaseChar(Arrays.asList("afdsafsa", "abc", "AcnaD", "A")));
     }
+
 }
---
+++ src/main/java/com/hikvision/rensu/lambda/chapter5/Excercise.java
@@ -0,0 +1,8 @@
+package com.hikvision.rensu.lambda.chapter5;
+
+/**
+ * Created by rensu on 17/3/19.
+ */
+public class Excercise {
+
+}
